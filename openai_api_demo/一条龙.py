9eb000230cff0be06f83dd00e1c2dae443fd89ad
xxxiu <129813663+morettt@users.noreply.github.com>
2024-03-04 09:58:45 +0800

    Update 一条龙.py

--- openai_api_demo/一条龙.py
+++ openai_api_demo/一条龙.py
@@ -1,10 +1,10 @@
 import subprocess
 
 # 运行脚本1
-subprocess.run(["python", "/root/ATAI Workshop/openai_api_demo/第一步处理文本.py"])
+subprocess.run(["python", "/root/ChatAT/openai_api_demo/第一步处理文本.py"])
 
 # 脚本1运行完成后，运行脚本2
-subprocess.run(["python", "/root/ATAI Workshop/openai_api_demo/第二步模型.py"])
+subprocess.run(["python", "/root/ChatAT/openai_api_demo/第二步模型.py"])
 
 # 脚本1运行完成后，运行脚本2
-subprocess.run(["python", "/root/ATAI Workshop/openai_api_demo/第三步处理QA.py"])
+subprocess.run(["python", "/root/ChatAT/openai_api_demo/第三步处理QA.py"])
