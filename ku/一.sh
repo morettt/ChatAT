229483e15c8a1b96661466f0ceb1035ec54f0969
xxxiu <129813663+morettt@users.noreply.github.com>
2024-03-24 17:05:03 +0800

    Update 一.sh

--- ku/一.sh
+++ ku/一.sh
@@ -3,8 +3,8 @@
 pkill -f "python openai_api-qwen.py"
 
 source activate py310_chat     
-cd /root/ChatAT/finetune_demo
+cd ../ChatAT/finetune_demo
 
-python finetune_hf.py data/ /root/autodl-tmp/chatglm3-6b configs/lora.yaml
+python finetune_hf.py data/ ../LLM-model/chatglm3-6b configs/lora.yaml
 
-python /root/ChatAT/finetune_demo/cli_demo.py
+python ../ChatAT/finetune_demo/cli_demo.py
