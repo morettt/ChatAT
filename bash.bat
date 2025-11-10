7c83c542d3d735c5432e0245f45e7513c9079b8b
xxxiu <shuaigew94@gmail.com>
2025-11-10 11:38:22 +0800

    update

--- bash.bat
+++ bash.bat
@@ -1,5 +1,9 @@
 @echo off
+chcp 65001 >nul
+echo ===== Git 快速提交 =====
 git add .
-git commit -m "更新代码"
+git commit -m "update"
+git pull origin main
 git push origin main
+echo ===== 推送完成！ =====
 pause
